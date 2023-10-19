70047351ece3d1012d03711fd7de39c2d3acc77f
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-19 13:13:22 +0300

    hover button white bg

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -76,7 +76,7 @@
     <a
       href="https://www.youtube.com/watch?v=H8ZH_mkfPUY&t=7s"
       target="_blank"
-      class="mx-auto px-4 py-2 rounded-md border border border-solid border-white
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white
     flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-teal-700 duration-200"
     >
       <i class="fa-regular fa-circle-play" />
