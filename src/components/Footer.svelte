da945a4ddcda60bc78b0c0654c2ab6f5717867aa
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-19 11:56:52 +0300

    also footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer
-  class="py-20 sm:py-32 bg-black border-t border-solid border-teal-950
+  class="py-20 sm:py-32 bg-stone border-t border-solid border-teal-950
 flex flex-col gap-4 sm:gap8 justify-center items-center"
 >
   <p class="px-4 py-2 bg-white text-stone-950 font-medium">
