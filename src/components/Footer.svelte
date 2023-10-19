d7eafcfbee6d0a58a4836c6dc51a5f742c2b6baf
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-19 10:40:52 +0300

    Footer finished

---
+++ src/components/Footer.svelte
@@ -0,0 +1,31 @@
+<footer
+  class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950
+flex flex-col gap-4 sm:gap8 justify-center items-center"
+>
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p><b class="pr-2">Email</b>Nicktvdd@gmail.com</p>
+    <p>
+      <b class="pr-2">Github</b>
+      <a class="text-violet-400" target="_blank" href="https://github.com/Nicktvdd">
+        Nicktvdd<sup><span class="text-xs scale-75 pl-0.5">
+          <i class="fa-solid fa-arrow-up-right-from-square text-xs
+          scale-[75%]"
+          />
+        </span></sup>
+      </a>
+    </p>
+    <p>
+      <b class="pr-2">LinkedIn</b>
+      <a class="text-violet-400" target="_blank" href="https://www.linkedin.com/in/nick-van-den-dungen/">
+        Nicktvdd<sup><span class="text-xs scale-75 pl-0.5">
+          <i class="fa-solid fa-arrow-up-right-from-square text-xs
+          scale-[75%]"
+          />
+        </span></sup>
+      </a>
+    </p>
+  </div>
+</footer>
