706b0d691886a60622a8b8b58efa565234ccbbb0
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-20 11:48:34 +0300

    typo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -30,7 +30,7 @@
     class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-stone-50 text-stone-950"
   >
     <div
-      class="absolute top-0 right-full w-full-h-full bg-teal-400 opacity-20 group-hover:transtone-x-full z-0 duration-200"
+      class="absolute top-0 right-full w-full-h-full bg-teal-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
     />
     <h4 class="relative z-9">Get in touch</h4>
   </a>
