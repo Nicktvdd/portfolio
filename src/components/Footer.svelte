146bc3266aae56ac1a18fcdf970eed8df01c7340
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-31 14:13:18 +0200

    Removed weird button footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,7 @@
 <footer
-  class="py-20 sm:py-32 bg-stone border-t border-solid border-teal-950
+  class="py-10 sm:py-16 bg-stone border-t border-solid border-teal-950
 flex flex-col gap-4 sm:gap8 justify-center items-center"
 >
-  <p class="px-4 py-2 bg-stone-50 text-stone-950 font-medium">
-    Connect with me &darr;
-  </p>
   <div class="flex flex-col gap-4 items-center justify-center">
     <p>
       <a 
